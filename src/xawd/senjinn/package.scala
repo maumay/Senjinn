95df28b5cdef8421478d304ec579372f3e277a69
Tom <t.oliver.ball@protonmail.ch>
2018-09-04 17:51:40 +0100

    removed redundant file + renamed BoardSquare.scala to boardsquare.scala

--- src/xawd/senjinn/package.scala
+++ src/xawd/senjinn/package.scala
@@ -5,4 +5,9 @@ package object senjinn
   type Sq = BoardSquare
   val  Sq = BoardSquare
   
+  val MaxNegatableInt: Int = Integer.MAX_VALUE - 1
+  val GameWinValue = MaxNegatableInt / 2
+  
+//  val InitialAlpha = -2 * (MaxNegatableInt / 3)
+//  val InitialBeta 
 }
