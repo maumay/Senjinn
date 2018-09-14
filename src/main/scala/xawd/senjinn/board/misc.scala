16a59d8493bf2fb4497cb714c61ab69c6997fb82
T <t@pop-os.localdomain>
2018-09-14 19:21:49 +0100

    Continuing impl

--- src/main/scala/xawd/senjinn/board/misc.scala
+++ src/main/scala/xawd/senjinn/board/misc.scala
@@ -62,10 +62,40 @@ object HashCache
 }
 
 import xawd.senjinn.CastleZone
+import xawd.senjinn.Side
 import scala.collection.{mutable => mutable}
 
 class CastlingTracker private(private val _rights: mutable.Set[CastleZone], 
     private var _white: Option[CastleZone], private var _black: Option[CastleZone])
 {
+    def rights = _rights
+    def white = _white
+    def black = _black
+    def status(side: Side) = if (side.isWhite) _white else _black
+    // def 
 
+    def remove(sideStatus: CastleZone) {
+        if (sideStatus.isWhiteZone) {
+            assert(_white.get == sideStatus)
+            _white = None
+        }
+        else {
+            assert(_black.get == sideStatus)
+            _black = None
+        }
+    }
+}
+
+object CastlingTracker
+{
+    private type TrackerCons = (Iterable[CastleZone], Option[CastleZone], Option[CastleZone])
+
+    def apply(cons: TrackerCons) = {
+        val (rights, white, black) = cons
+        new CastlingTracker(mutable.HashSet(rights.iterator.toSeq: _*), white, black)
+    }
+
+    def apply(): CastlingTracker = {
+        apply(mutable.HashSet(CastleZone.values: _*), None, None)
+    }
 }
