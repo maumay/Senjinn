1c74cba50ecbe7fefe3f7d3190401981c9252f07
Tom <t.oliver.ball@protonmail.ch>
2018-10-27 16:58:18 +0100

    working on test file parsing code

--- src/test/scala/parsers/ChessRegex.scala
+++ src/test/scala/parsers/ChessRegex.scala
@@ -3,7 +3,7 @@ import scala.util.matching.Regex
 
 /**
  */
-object ChessRegex extends App
+object ChessRegex //extends App
 {
   private val arrow: String = "\\-\\>"
   
@@ -11,9 +11,12 @@ object ChessRegex extends App
   val doubleSquare: Regex = s"($singleSquare +$singleSquare)".r
   val cord: Regex = s"($singleSquare$arrow$singleSquare)".r
   val multiTarget: Regex = s"$singleSquare$arrow\\{( *$singleSquare *)\\}".r
+  val shorthandMove: Regex = raw"([sScCeEpP]\[[a-hA-H1-8kKqQwW \-\>\{\}]+( [NBRG])?\])".r
+  val castleZone = "(([wW][kK])|([wW][qQ])|([bB][kK])|([bB][qQ]))".r
   
-  "e4->h6" match {
-    case cord(_*) => println("matched")
-    case _ => println("failed")
-  }
+  
+//  "e4->h6" match {
+//    case cord(_*) => println("matched")
+//    case _ => println("failed")
+//  }
 }
