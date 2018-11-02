0474e8daae58fe5fc80b8df7f8a9ce57574e3c50
Tom <t.oliver.ball@protonmail.ch>
2018-11-02 22:05:29 +0000

    Started testing mechanism of board evolution

---
+++ src/test/scala/moves/EvolutionTest.scala
@@ -0,0 +1,12 @@
+package moves
+
+import org.scalatest.FlatSpec
+import senjinn.parsers.MoveParsing
+import senjinn.parsers.BoardParsing
+
+/**
+ */
+class EvolutionTest extends FlatSpec with MoveParsing with BoardParsing
+{
+  
+}
