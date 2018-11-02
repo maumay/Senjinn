568c685a62f40c6aef1ff55d5e9f800e83d15717
Tom <t.oliver.ball@protonmail.ch>
2018-11-02 22:41:40 +0000

    Implementing the move test

--- src/test/scala/moves/EvolutionTest.scala
+++ src/test/scala/moves/EvolutionTest.scala
@@ -3,10 +3,34 @@ package moves
 import org.scalatest.FlatSpec
 import senjinn.parsers.MoveParsing
 import senjinn.parsers.BoardParsing
+import senjinn.base.{loadResource}
+import senjinn.board.{ BoardState }
+import senjinn.moves.{ ChessMove, StandardMove, CastleMove, EnpassantMove, PromotionMove }
 
 /**
  */
 class EvolutionTest extends FlatSpec with MoveParsing with BoardParsing
 {
-  
+  val testpkg: Package = getClass.getPackage
+  type TestCaseArgs = (ChessMove, BoardState, BoardState)
+
+  //  def testCaseIterator: Iterator[TestCaseArgs] = {
+  //    val zeropad = (n: Int) => ('0' * n.toString.length - 1) + n.toString
+  //    (0 until 40) map { zeropad }
+  //    map { name => loadResource( testpkg, name) }
+  //    map { lines => (parseMove(lines()
+  //  }
+  //
+
+  private def parseMove(encoded: String): ChessMove = {
+    import senjinn.parsers.ChessRegex.{ castleZone => czregex }
+    val (src, target) = "[a-h][1-8]".r.findAllIn(encoded).toVector.splitAt(1)
+    encoded.toLowerCase.head match {
+      case 's' => StandardMove(src(0), target(0))
+      case 'e' => EnpassantMove(src(0), target(0))
+      case 'c' => CastleMove(czregex.findFirstIn(encoded).get)
+      case 'p' => PromotionMove(src(0), target(0), "[nbrq] ".r.findFirstIn(encoded).get)
+      case _   => throw new RuntimeException
+    }
+  }
 }
