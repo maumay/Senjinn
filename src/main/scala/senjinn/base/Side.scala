772276b2067a3af9d9158ad2e1b6de69b826e1f9
Tom <tom.oliver.ball@gmail.com>
2019-01-18 20:15:02 +0000

    Documented the Side enum

--- src/main/scala/senjinn/base/Side.scala
+++ src/main/scala/senjinn/base/Side.scala
@@ -2,19 +2,30 @@ package senjinn.base
 
 import enumeratum._
 
-
+/**
+ * Represents the two possible sides in a chess game; white and black.
+ */
 sealed abstract class Side(val isWhite: Boolean) extends EnumEntry
 {
+  /** 
+   * The index of the rank a pawn on this side must be on in order for it to get
+   * promoted on it's next move.
+   */
   val penultimatePawnRank = if (isWhite) 6 else 1
 }
 
 object Side extends Enum[Side]
 {
+  /**
+   * Returns the opposing side to the given parameter.
+   * @param side the color to find the enemy of.
+   */
   def other(side: Side): Side = side match {
     case White => Black
     case Black => White
   }
   
+  /** A vector containing both sides. */
   val values = findValues.toVector
   
   case object White extends Side(true)
